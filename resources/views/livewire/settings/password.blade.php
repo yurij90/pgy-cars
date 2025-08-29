89ba944b97b17b2bf02c7e5144ad1becbc5008b7
Pataki György <wolfdog90@gmail.com>
2025-08-29 17:32:54 +0200

    Password rules set

--- resources/views/livewire/settings/password.blade.php
+++ resources/views/livewire/settings/password.blade.php
@@ -19,7 +19,7 @@ new class extends Component {
         try {
             $validated = $this->validate([
                 'current_password' => ['required', 'string', 'current_password'],
-                'password' => ['required', 'string', Password::defaults(), 'confirmed'],
+                'password' => ['required', 'string', Password::min(8)->mixedCase()->numbers()->symbols(), 'confirmed'],
             ]);
         } catch (ValidationException $e) {
             $this->reset('current_password', 'password', 'password_confirmation');
