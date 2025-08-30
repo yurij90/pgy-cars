e73ed158fe0da3fcfa6180b2089524e48e05894a
Pataki György <wolfdog90@gmail.com>
2025-08-30 09:31:00 +0200

    WinGet check added to install.ps1

--- winget.ps1
+++
@@ -1,5 +0,0 @@
-$progressPreference = 'silentlyContinue'
-Write-Host "Installing WinGet PowerShell module from PSGallery..."
-Install-PackageProvider -Name NuGet -Force | Out-Null
-Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
-Write-Host "Done."
